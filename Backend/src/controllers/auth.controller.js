8681f7c91efbb68f21022249a066df20f4e05a3c
Ali Ahmed <aliahmed193077@gmail.com>
2025-11-11 19:10:48 +0500

    Auth Route completed

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -127,3 +127,13 @@ export const updateProfile = async() =>{
     }
 
 }
+
+//it is for if on refreshing we want to show login page or the profile it is authentication check 
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in chechAuth controller", error.message);
+        res.status(500).json({message: "Internal Server Error"})
+    }
+}
