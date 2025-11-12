cd0b54f53a05a25b9febfabb99365ff553aa59ba
Ali Ahmed <aliahmed193077@gmail.com>
2025-11-12 18:09:54 +0500

    Backend completed for now moving to the react app

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js"
 
 
 export const getUsersForSidebar  = async(req, res) =>{
@@ -50,5 +51,44 @@ export const getMessages = async(req, res)=>{
         console.log("Error in getMessages controller", error.message)
         res.status(500).json({error: "Internal Server Error"})
         
+    }
+}
+
+//sending the message functionality it could be text, it could be image
+export const sendMessage = async(req, res)=>{
+
+    try {
+            const {text, image}= req.body   
+            const {id: receiverId} = req.params    
+
+            const senderId = req.user._id
+
+            let imageUrl
+            if(image){
+                //upload base64 image to cloudinary
+                const uploadResponse = await cloudinary.uploader.upload(image);
+                imageUrl = uploadResponse.secure_url;
+
+            }
+
+            //now message case
+
+            const newMessage = new Message({
+                senderId,
+                receiverId,
+                text,
+                image: imageUrl,
+            })
+
+            await newMessage.save()
+
+            //todo: real time functionality foes here => socket.io
+            
+            res.status(201).json({newMessage})
+            
+
+    } catch (error) {
+        console.log("Error in sendMessages controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
     }
 }
